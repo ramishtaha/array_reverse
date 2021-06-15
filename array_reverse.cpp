0ede476c8a3525d9b7ce159238d65ddeea41f12c
ramishtaha <trixterthe@gmail.com>
2021-06-15 18:00:31 +0530

    version 2

--- array_reverse.cpp
+++ array_reverse.cpp
@@ -14,6 +14,7 @@ int main() {
     for(int i = 0; i < size; i++){
         cin >> arr[i];
     }
+    cout << "Array in Reverse form is.\n"
     for(int i = size-1 ; i >= 0; i--){
         cout << arr[i]<< " ";
     }
